3363edb13bd6b39a82835d800c8e1b027a57f5dd
DrPawat <88647474+DrPawat@users.noreply.github.com>
2023-11-12 12:37:55 +0700

    first commit

---
+++ pratice_problem/ex1_dna.py
@@ -0,0 +1,22 @@
+#my_solution
+
+# def DNA_strand(dna):
+#     ans=[]
+#     for i in dna:
+#         if i == 'A':
+#             ans.append('T')
+#         if i == 'T':
+#             ans.append('A')
+#         if i == 'C':
+#             ans.append('G')
+#         if i == 'G':
+#             ans.append('C')
+#     return ''.join(ans)
+
+#better solution
+
+pairs = {'A':'T','T':'A','C':'G','G':'C'}
+def DNA_strand(dna):
+    return ''.join([pairs[x] for x in dna])
+
+print(DNA_strand('AAA'))
